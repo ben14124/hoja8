a53ea042358d536dcecf4cf4d687af909091b622
dbs_jd <dbs_jd@172.20.15.55>
2015-10-02 13:53:54 -0600

    Java Collection Framework

--- hoja8/src/hoja8/Paciente.java
+++ hoja8/src/hoja8/Paciente.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Hoja de trabajo 8
+ * Heap
+ * Juan Diego Benitez - 14124
+ * Daniela Pocasangre - 14612
  */
 
 package hoja8;
@@ -35,17 +36,26 @@ public class Paciente implements Comparable<Paciente> {
     public char getLetra() {
         return letra;
     }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + enfermedad + ", " + letra;
+    }
+    
+    
     
     @Override
     public int compareTo(Paciente otroPaciente) {
         int mandar = 0;
         char letraOtro = otroPaciente.getLetra();
         if(letra > letraOtro){
-            mandar = 1;
+            return 1;
+            
         }
-        else{
-            mandar = 0;
+        else if (letra == letraOtro){
+           return 0;
+            
         }
-        return mandar;
+        return -1;
     }
 }
